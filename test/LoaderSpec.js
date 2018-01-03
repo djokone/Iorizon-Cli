f85554890c779eecdf46c628067871b53567c217
DjOkOne <tim.potter@wanadoo.fr>
2018-01-04 00:09:23 +0100

    :umbrella::microscope::construction: Describe  Loader Initialization
    Describe all kind of initialization we can do with that primary object

--- test/LoaderSpec.js
+++ test/LoaderSpec.js
@@ -1,26 +1,63 @@
 let Loader = require('../bin/lib/Loader.js')
 var chai = require('chai')
+var path = require('path')
 var expect = chai.expect
 var should = chai.should
 let customLoader = new Loader('./test/ioTest.json')
+
 describe('Loader class', function () {
+
+  let PathLoader, ObjectLoader, NudeLoader, AbsPathLoader
+  let loaders = {}
+
   describe('#constructor (toLoad = false, config = {})', function () {
-    let PathLoader, ObjectLoader, NudeLoader
     describe('Init with relative path', function () {
       PathLoader = new Loader('./test/ioTest.json')
-      it('Should instantiate with the right properties', function () {
+      it('Should init corectly', function () {
         expect(PathLoader.ioFileName, 'Problem to get file from path').equal('ioTest.json')
         expect(PathLoader.type, 'Should have a relativePath type').equal('relativePath')
       })
       it('Should load the file', function () {
-        expect(PathLoader.type, 'Should have a relativePath type').equal('relativePath')
         expect(PathLoader.isFileLoaded).to.be.true
+        expect(PathLoader.isLoaded).to.be.true
       })
+      loaders['RelPath'] = {
+        loader: PathLoader,
+        it: 'Should work with relative path'
+      }
     })
+
+    describe('Init with absolute path', function () {
+      AbsPathLoader = new Loader(path.resolve(__dirname, 'ioTest.json'))
+      it('Should init corectly', () => {
+        expect(AbsPathLoader.type).equal('absolutePath')
+      })
+      it('Should load the file', () => {
+        expect(AbsPathLoader.isLoaded).to.be.true
+        expect(AbsPathLoader.isFileLoaded).to.be.true
+      })
+      loaders['AbsPath'] = {
+        loader: AbsPathLoader,
+        it: 'Should work with absolute path'
+      }
+    })
+
     describe('Init with nothings', function () {
+
+        // in the current directory of the processus
+      let result = path.resolve(__dirname, '../') 
+      
       NudeLoader = new Loader()
-      it('Should find a path', function () {
-        
+      it('Should init corectly', function () { 
+        expect(NudeLoader.autoRun).to.be.true
+      })
+      it('Should find the default path file', function () {
+        expect(NudeLoader.ioFileName).equal('io.json')
+        expect(NudeLoader.defaultPath).equal(result)
+      })
+      it('Should load io.json in the current directory', function () {
+        expect(NudeLoader.isFileLoaded).to.be.true
+        expect(NudeLoader.isLoaded).to.be.true
       })
     })
 
@@ -38,14 +75,35 @@ describe('Loader class', function () {
     })
 
     describe('Init with object', function () {
-      let ObjectLoader = new Loader({name: 'Iotest'})
-      it('Should ', function () {
+      let data = {
+        name: 'Iotest'
+      }
+      let ObjectLoader = new Loader(data)
+      it('Should init corectly', function () {
         expect(ObjectLoader.isFileLoaded).to.be.false
+        expect(ObjectLoader.type).equal('object')
+      })
+      it('Should load data', function () {
+        expect(ObjectLoader.isLoaded).to.be.true
+        expect(ObjectLoader.content).equal(data)
       })
       
     })
-    
+    loaders['obj'] = {
+      loader: ObjectLoader,
+      it: 'Should work with data init'
+    }
   })
-  describe.skip('# set content(val)', () => {
+  describe('# set content(val)', () => {
+    for (let data in loaders) {
+      let ref = loaders[data]
+      it(ref.it, () => {
+        ref.loader.content = { name: 'test' }
+        expect(ref.loader.reloaded).equal(1)
+      })
+    }
+  })
+
+  describe.skip('# load (path, auto = true)', () => {
   })
 })
